dd098af430bca83b6b6cf853949d3d7e703f329b
EC2 Default User <ec2-user@ip-172-31-1-91.us-east-2.compute.internal>
2018-10-30 06:16:39 +0000

    commented the code

--- src/h1B_counting.scala
+++ src/h1B_counting.scala
@@ -3,11 +3,25 @@ import scala.collection.mutable.{Map,HashMap,PriorityQueue}
 import java.io._
 
 object H1BStats {
+  
+  /*
+  Suppose filter_on is [(CASE_STATUS,CERTIFIED),(EMPLOYER_STATE,CA)] which give the criterion for whether a worker is counted
+  or not and headers is the array of column titles in input.csv. One of those indices is for CASE_STATUS (say i) and one is EMPLOYER_STATE (j)
+  This function then returns [(i,CERTIFIED),(j,CA)]. If we have some other criterion like (JUNK,DONT_CARE), then
+  JUNK is not one of the headers so it isn't found. In that case instead of having an entry (-1,DONT_CARE), that criterion is removed.
+  */
   def make_filter_on(filter_on: List[Tuple2[String,String]], headers: Array[String]): List[Tuple2[Int,String]]={
     var return_value=filter_on.map(filter_on_variable=>(headers.indexOf(filter_on_variable._1),filter_on_variable._2))
     return_value=return_value.filterNot(x=>(x._1==(-1)))
     return_value
   }
+  /*
+  The previous function has made a list of criterion. For a single one of them, this checks if the line of input
+  representing a single worker as current_line satisfies that criterion.
+  That is if the criterion says the i'th entry of this row should say CERTIFIED, then this checks that.
+  In case, the current_line does not have an i'th entry, it is treated as false. This should only happen
+  if there is a mismatch in number of columns as compared with the header line of input.csv
+  */
   def meets_criterion(current_line: Array[String], criterion: Tuple2[Int,String]) : Boolean ={
     var return_value=false
     try{
@@ -18,9 +32,18 @@ object H1BStats {
     }
     return_value
   }
+  /*
+  Combine the information from all the critiria to see if the worker does or does not meet all of them.
+  */
   def meets_criteria(current_line: Array[String], criteria: List[Tuple2[Int,String]]) : Boolean ={
     criteria.forall(z=>meets_criterion(current_line,z))
   }
+  /*
+  entries is the current_line for a given worker
+  current_dict is (i,dictionary) where i represents the column index of one of the K different things we are counting.
+  the dictionary is the one keeping track of counts for that variable.
+  we look at entries[i] to see something like this worker is a ENGINEER and increment the count of ENGINEER by 1
+  */
   def modify_for_this_line(entries: Array[String],current_dict: Tuple2[Int,Map[String,Int]]): Unit={
     try{
       val current_var_for_this_line=cleanString(entries(current_dict._1))
@@ -33,7 +56,11 @@ object H1BStats {
       }
     }
   }
-  
+  /*
+  Open the file and read line by line. Check if this line is valid using the meets_criteria function.
+  Then for each of the K variables being counted use the modify_for_this_line function on the given line.
+  Repeat for all the workers representing individual lines in the input_file
+  */
   def make_dicts(input_file: String, filter_on: List[Tuple2[String,String]], variables_to_count: List[String]) : (List[Map[String,Int]],Int) = {
     val bufferedSource=Source.fromFile(input_file)
     val headers = bufferedSource.getLines.next().split(";")
@@ -54,15 +81,29 @@ object H1BStats {
     bufferedSource.close
     (indices_and_all_the_dicts.map(z=>z._2),total_to_count)
   }
+  /*Remove extraneous quotation marks*/
   def cleanString(unformated_string: String) : String={
     unformated_string.replace("\"","")
   }
+  /*
+  Take each dictionary that says for example (ENGINEER,5),(PROFESSOR,1),(ADMINISTRATOR,5) and put them into a heap
+  so that the 5 administrators are at the top because they are tied with engineers with the most, but beat them in
+  alphabetical order.
+  */
   def make_heap(this_dict : Map[String,Int]) : PriorityQueue[Tuple2[Int,String]] = {
     val ord:Ordering[(Int,String)] = Ordering.Tuple2(Ordering.Int,Ordering.String.reverse)
     val pq=new PriorityQueue[Tuple2[Int,String]]()(ord)
     val seq_to_add=this_dict.toIndexedSeq.map(z=>(z._2,cleanString(z._1)))
     pq++seq_to_add
   }
+  /*
+  give this_output as combining the information of the header for the first column representing what was being counted.
+  along with the location of the file where we are supposed to write along with the PriorityQueue constructed by the make_heap
+  also provide how many workers were counted by meeting all the criteria and the top_how_many that should be written to the file.
+  Then one by one dequeue from the PriorityQueue and write the line with the associated string, the count and count/total_counted as
+  a percentage.
+  When the queue is empty and throws an error when being dequeue'd we stop.
+  */
   def write_output(this_output: Tuple2[Tuple2[String,String],PriorityQueue[Tuple2[Int,String]]], total_counted: Int, top_how_many : Int) : Unit={
     val ((output_file,header),pq)=this_output
     val file=new File(output_file)
@@ -83,7 +124,9 @@ object H1BStats {
   }
   
   /*
-  
+  Build the dictionaries that count the K variables with the make_dicts function.
+  Turn each of them into heaps with make_heap
+  Then use write_output on each of the K output files
   */
   def main(args: Array[String]): Unit = {
     //val criteria=List(("CASE_STATUS","CERTIFIED"),("UNUSED","DONT CARE"))
